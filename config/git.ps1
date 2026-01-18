906dff95d252d50b5adf7c9b32685b9548a99a80
Joel Cutler <joelcutler108@gmail.com>
2026-01-18 16:36:58 +0100

    add git config

---
+++ config/git.ps1
@@ -0,0 +1,5 @@
+# todo these should be generic and passed?
+git config --global user.name "Joel Cutler"
+git config --global user.email "joelcutler108@gmail.com"
+git config --global core.autocrlf true
+git config --global pull.rebase true
