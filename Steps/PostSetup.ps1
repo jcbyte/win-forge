13d40edf267fb6e269701c86a09a647d6c6c6f6d
Joel Cutler <joelcutler108@gmail.com>
2026-01-28 17:23:36 +0000

    merge write todo function into one block

--- Steps/PostSetup.ps1
+++ Steps/PostSetup.ps1
@@ -2,29 +2,35 @@
 
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-function Write-Todo ([string]$Title, [string]$ActionText = $null, [ScriptBlock]$Action = $null) {
-  Write-Host "   $Title " -NoNewline -ForegroundColor Yellow
-  [Console]::ReadKey() | Out-Null
-  Write-Host "`r✅ $Title" -ForegroundColor DarkGray 
-}
-
-function Write-Todo-Action ([string]$Title, [string]$ActionText, [ScriptBlock]$Action ) {
+function Write-Todo ([string]$Title, [string]$ActionText, [ScriptBlock]$Action ) {
   Write-Host "   $Title" -NoNewline -ForegroundColor Yellow
-  Write-Host " - $ActionText [y?]" -NoNewline -ForegroundColor DarkGray
 
-  $key = [Console]::ReadKey($true)
-  if ($key.Key -in @('y', 'Y')) {
-    Write-Host "`r   $Title" -ForegroundColor DarkGray 
-    & $Action
+  if ($Action -ne $null) {
+    # Throw is provided an action without description
+    if (-not $ActionText) {
+      Throw "ActionText is required when Action is provided."
+    }
+    
+    # If action is provided, ask for confirmation
+    Write-Host " - $ActionText [y?]" -NoNewline -ForegroundColor DarkGray
+
+    $key = [Console]::ReadKey($true)
+    if ($key.Key -in @('y', 'Y')) {
+      Write-Host "`r   $Title" -ForegroundColor DarkGray 
+      & $Action
+    }
+  }
+  else {
+    # Otherwise wait for any key to be pressed to confirm
+    [Console]::ReadKey() | Out-Null
+
   }
 
   Write-Host "`r✅ $Title" -ForegroundColor DarkGray 
 }
 
 # Remind user to Activate Windows, using advanced MAS Troubleshooting if required
-Write-Todo-Action "Activate Windows (and Office)" "Start troubleshooting: https://massgrave.dev/" {
-  irm https://get.activated.win | iex
-}
+Write-Todo "Activate Windows (and Office)" "Open troubleshooting: https://massgrave.dev/" { irm https://get.activated.win | iex }
 
 # Remind User to Sign In to installed Applications
 Write-Todo "Sign In to Chrome"
