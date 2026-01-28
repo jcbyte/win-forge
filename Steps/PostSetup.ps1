179e2362a6bf682be9cf43ea6311410fcacbf303
Joel Cutler <joelcutler108@gmail.com>
2026-01-28 17:16:19 +0000

    open MAS troubleshooting if user requests

--- Steps/PostSetup.ps1
+++ Steps/PostSetup.ps1
@@ -2,15 +2,31 @@
 
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-function Write-Todo ([string]$Title) {
+function Write-Todo ([string]$Title, [string]$ActionText = $null, [ScriptBlock]$Action = $null) {
   Write-Host "   $Title " -NoNewline -ForegroundColor Yellow
   [Console]::ReadKey() | Out-Null
   Write-Host "`r✅ $Title" -ForegroundColor DarkGray 
 }
 
-# Todo could this run "automatically" if approving
-Write-Todo "Activate Windows (and Office), for troubleshooting: ``irm https://get.activated.win | iex``"
+function Write-Todo-Action ([string]$Title, [string]$ActionText, [ScriptBlock]$Action ) {
+  Write-Host "   $Title" -NoNewline -ForegroundColor Yellow
+  Write-Host " - $ActionText [y?]" -NoNewline -ForegroundColor DarkGray
 
+  $key = [Console]::ReadKey($true)
+  if ($key.Key -in @('y', 'Y')) {
+    Write-Host "`r   $Title" -ForegroundColor DarkGray 
+    & $Action
+  }
+
+  Write-Host "`r✅ $Title" -ForegroundColor DarkGray 
+}
+
+# Remind user to Activate Windows, using advanced MAS Troubleshooting if required
+Write-Todo-Action "Activate Windows (and Office)" "Start troubleshooting: https://massgrave.dev/" {
+  irm https://get.activated.win | iex
+}
+
+# Remind User to Sign In to installed Applications
 Write-Todo "Sign In to Chrome"
 Write-Todo "Sign In to Spotify"
 Write-Todo "Sign In to WhatsApp"
