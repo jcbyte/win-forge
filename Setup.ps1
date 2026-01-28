89abc4005d53cbc3c79db1fac6caff6bf1bd412c
Joel Cutler <joelcutler108@gmail.com>
2026-01-28 14:16:34 +0000

    place retrieved file in a temporary dir instead of file

--- Setup.ps1
+++ Setup.ps1
@@ -1,20 +1,6 @@
 Read-Host "Continue"
 
-# Self-elevate the script if required
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
 
-  $setupFile = New-TemporaryFile
-  Invoke-RestMethod "https://raw.githubusercontent.com/jcbyte/win-forge/refs/heads/main/Setup.ps1" -OutFile $setupFile
-
-  Start-Process -FilePath PowerShell.exe -ArgumentList "-NoExit -File `"$setupFile`" -NoProfile -ExecutionPolicy Bypass" -Verb Runas
-  Read-Host "Hold Until Enter here? $setupFile"
-  Exit
-}
-
-Read-Host "This is admin..."
-
-$REPO_NAME = "win-forge"
-$REPO_URL = "https://github.com/jcbyte/$REPO_NAME.git"
 
 function New-TemporaryDirectory {
   $tmp = [System.IO.Path]::GetTempPath()
@@ -23,6 +9,25 @@ function New-TemporaryDirectory {
   New-Item -ItemType Directory -Path $path
   return $path
 }
+
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+
+  $tempSetupDir = New-TemporaryDirectory
+  $tempSetupFile = Join-Path $tempSetupDir "Setup.ps1"
+  Invoke-RestMethod "https://raw.githubusercontent.com/jcbyte/win-forge/refs/heads/main/Setup.ps1" -OutFile "$tempSetupFile"
+
+  Start-Process -FilePath PowerShell.exe -ArgumentList "-NoExit -File `"$tempSetupFile`" -NoProfile -ExecutionPolicy Bypass" -Verb Runas
+  Read-Host "Hold Until Enter here? $tempSetupFile"
+  Exit
+}
+
+Read-Host "This is admin..."
+
+$REPO_NAME = "win-forge"
+$REPO_URL = "https://github.com/jcbyte/$REPO_NAME.git"
+
+
 
 # Install Git early to clone the repo
 winget install -e --id Git.Git
