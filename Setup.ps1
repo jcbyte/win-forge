00b7d8bf7cda3b92c3d77ad0066497a72724e4c8
Joel Cutler <joelcutler108@gmail.com>
2026-01-28 13:45:05 +0000

    clone git repo

--- Setup.ps1
+++ Setup.ps1
@@ -1,4 +1,31 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
+$REPO_NAME = "win-forge"
+$REPO_URL = "https://github.com/jcbyte/$REPO_NAME.git"
+
+function New-TemporaryDirectory {
+  $tmp = [System.IO.Path]::GetTempPath()
+  $name = (New-Guid).ToString("N")
+  $path = Join-Path $tmp $name
+  New-Item -ItemType Directory -Path $path
+  return $path
+}
+
+# Install Git early to clone the repo
+winget install -e --id Git.Git
+
+$tempDir = New-TemporaryDirectory
+Set-Location "$tempDir"
+git clone "$REPO_URL"
+
+$repoDir = Join-Path $tempDir $REPO_NAME
+if (-not (Test-Path $repoDir)) {
+  Write-Error "Failed to fetch Repo!"
+  exit 1
+}
+Set-Location "$repoDir"
+
+# Execute
+
+# Set-ExecutionPolicy Bypass -Scope Process -Force
 
 .\ConfigureWindows.ps1
 .\InstallPackages.ps1
