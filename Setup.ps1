77f8aa4148193eb687a353a123f7855c99640306
Joel Cutler <joelcutler108@gmail.com>
2026-01-28 16:06:01 +0000

    clone repository and call pipeline script

--- Setup.ps1
+++ Setup.ps1
@@ -1,3 +1,5 @@
+# Setup Script which should be run, to gain elevated privileges and download the repository
+
 # Set constants
 $REPO_NAME = "win-forge"
 $REPO_URL = "https://github.com/jcbyte/$REPO_NAME.git"
@@ -6,58 +8,42 @@ $SETUP_SCRIPT_URL = "https://raw.githubusercontent.com/jcbyte/$REPO_NAME/refs/he
 
 # Creates a new temporary directory and returns its path
 function New-TemporaryDirectory {
-  $tmp = [System.IO.Path]::GetTempPath()
-  $name = (New-Guid).ToString("N")
-  $path = Join-Path $tmp $name
-  New-Item -ItemType Directory -Path $path | Out-Null
-  return $path
+  $TmpDir = [System.IO.Path]::GetTempPath()
+  $Name = (New-Guid).ToString("N")
+  $Path = Join-Path $TmpDir $Name
+  New-Item -ItemType Directory -Path $Path | Out-Null
+  return $Path
 }
 
 # Check if the script is running as administrator; if not, relaunch it with elevation
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-  $cmdPath = $PSCommandPath
+  $CmdPath = $PSCommandPath
   # If the script is running directly from memory (`irm`), download it to a temporary file
-  if (-not $cmdPath) {
-    $tempSetupDir = New-TemporaryDirectory
-    $tempSetupFile = Join-Path $tempSetupDir $SETUP_SCRIPT
-    Invoke-RestMethod $SETUP_SCRIPT_URL -OutFile $tempSetupFile
-    $cmdPath = $tempSetupFile
+  if (-not $CmdPath) {
+    $TempSetupDir = New-TemporaryDirectory
+    $TempSetupFile = Join-Path $TempSetupDir $SETUP_SCRIPT_NAME
+    Invoke-RestMethod $SETUP_SCRIPT_URL -OutFile $TempSetupFile
+    $CmdPath = $TempSetupFile
   }
 
   # Relaunch the script with elevated privileges
-  Start-Process -FilePath PowerShell.exe -ArgumentList "-NoExit -NoProfile -ExecutionPolicy Bypass -File `"$cmdPath`"" -Verb Runas
+  Start-Process -FilePath PowerShell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$CmdPath`"" -Verb Runas
   Exit
 }
 
-Read-Host "We Are Admin!"
-# todo below
-
-
-
-
-
 # Install Git early to clone the repo
-winget install -e --id Git.Git
+Write-Host "Installing Git"
+winget install -e --id Git.Git --silent --accept-source-agreements --accept-package-agreements
 
-$tempDir = New-TemporaryDirectory
-Set-Location "$tempDir"
-git clone "$REPO_URL"
+# Clone repository
+Write-Host "Cloning ``$REPO_NAME`` Repository"
+$RepoDir = New-TemporaryDirectory
+git clone "$REPO_URL" "$RepoDir" --quiet
 
-$repoDir = Join-Path $tempDir $REPO_NAME
-if (-not (Test-Path $repoDir)) {
-  Write-Error "Failed to fetch Repo!"
-  exit 1
-}
-Set-Location "$repoDir"
+# Execute the setup pipeline
+$SetupPipelineScript = Join-Path $RepoDir "SetupPipeline.ps1"
+& $SetupPipelineScript $RepoDir
 
-# Execute
-
-# Set-ExecutionPolicy Bypass -Scope Process -Force
-
-.\ConfigureWindows.ps1
-.\InstallPackages.ps1
-.\ConfigurePackages.ps1
-.\InstallDev.ps1
-.\PostSetup.ps1
-
-Read-Host "Hold Until Enter"
+# Wait for user input before closing
+Write-Host "Setup Complete. Press Enter to Exit" -NoNewline
+[Console]::ReadKey() | Out-Null
