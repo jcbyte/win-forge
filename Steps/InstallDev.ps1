ee4722e7514e6f2e36b9e848a99cca2b9ac3c446
Joel Cutler <joelcutler108@gmail.com>
2026-01-28 22:41:58 +0000

    setup node and python in dev

--- Steps/InstallDev.ps1
+++ Steps/InstallDev.ps1
@@ -1,8 +1,15 @@
 # Install Dev
 
-Write-Host "Install Dev"
-# todo
+# Install and use latest Node.js LTS
+Write-Host "Installing Latest Node.js LTS"
+nvm install lts
+nvm use lts
 
-# Todo Install Node.js
-# Todo Install Python
-# Todo Install Rust? How does rustup work?
+# Install latest Python3
+Write-Host "Installing Latest Python 3"
+py install 3
+# Add pythons global shortcuts directory to PATH
+$PythonBin = "$env:LOCALAPPDATA\Python\bin"
+$path = [Environment]::GetEnvironmentVariable("PATH", "User")
+$newPath = "$path;$PythonBin"
+[Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
