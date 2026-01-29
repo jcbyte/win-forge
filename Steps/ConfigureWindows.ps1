c64a864823b10ea4abd05e22d4410648d9f932a4
Joel Cutler <joelcutler108@gmail.com>
2026-01-29 18:00:39 +0000

    use Win11Debloat to debloat and configure

--- Steps/ConfigureWindows.ps1
+++ Steps/ConfigureWindows.ps1
@@ -1,7 +1,120 @@
 # Windows Configuration and De-bloating
-# todo
+# todo doc
 
-Write-Host "Configure Windows"
+Write-Host "🛠️ Configuring and Debloating Windows using" -NoNewline
+Write-Host " Win11Debloat" -NoNewline -ForegroundColor Cyan
+Write-Host " (https://github.com/Raphire/Win11Debloat)" -ForegroundColor DarkGray
 
-# Todo Debloat + Configure Windows (https://github.com/Raphire/Win11Debloat)
+$RemoveApps = @(
+  # Default removal - Microsoft apps
+  "Clipchamp.Clipchamp",
+  "Microsoft.3DBuilder",
+  "Microsoft.549981C3F5F10",
+  "Microsoft.BingFinance",
+  "Microsoft.BingFoodAndDrink",
+  "Microsoft.BingHealthAndFitness",
+  "Microsoft.BingNews",
+  "Microsoft.BingSports",
+  "Microsoft.BingTranslator",
+  "Microsoft.BingTravel",
+  "Microsoft.Copilot",
+  "Microsoft.Messaging",
+  "Microsoft.MicrosoftJournal",
+  "Microsoft.MicrosoftOfficeHub",
+  "Microsoft.MicrosoftPowerBIForWindows",
+  "Microsoft.MicrosoftSolitaireCollection",
+  "Microsoft.MicrosoftStickyNotes",
+  "Microsoft.MixedReality.Portal",
+  "Microsoft.NetworkSpeedTest",
+  "Microsoft.News",
+  "Microsoft.Office.OneNote",
+  "Microsoft.Office.Sway",
+  "Microsoft.OneConnect",
+  "Microsoft.Print3D",
+  "Microsoft.SkypeApp",
+  "Microsoft.Todos",
+  "Microsoft.Windows.DevHome",
+  "Microsoft.WindowsFeedbackHub",
+  "Microsoft.WindowsMaps",
+  "Microsoft.WindowsSoundRecorder",
+  "Microsoft.XboxApp",
+  "Microsoft.ZuneVideo",
+  "MicrosoftCorporationII.MicrosoftFamily",
+  "MicrosoftTeams",
+  "MSTeams",
+  # Default removal - Third party apps
+  "ActiproSoftwareLLC",
+  "AdobeSystemsIncorporated.AdobePhotoshopExpress",
+  "Amazon.com.Amazon",
+  "AmazonVideo.PrimeVideo",
+  "Asphalt8Airborne ",
+  "AutodeskSketchBook",
+  "CaesarsSlotsFreeCasino",
+  "COOKINGFEVER",
+  "CyberLinkMediaSuiteEssentials",
+  "DisneyMagicKingdoms",
+  "Disney",
+  "DrawboardPDF",
+  "Duolingo-LearnLanguagesforFree",
+  "EclipseManager",
+  "Facebook",
+  "FarmVille2CountryEscape",
+  "fitbit",
+  "Flipboard",
+  "HiddenCity",
+  "HULULLC.HULUPLUS",
+  "iHeartRadio",
+  "Instagram",
+  "king.com.BubbleWitch3Saga",
+  "king.com.CandyCrushSaga",
+  "king.com.CandyCrushSodaSaga",
+  "LinkedInforWindows",
+  "MarchofEmpires",
+  "Netflix",
+  "NYTCrossword",
+  "OneCalendar",
+  "PandoraMediaInc",
+  "PhototasticCollage",
+  "PicsArt-PhotoStudio",
+  "Plex",
+  "PolarrPhotoEditorAcademicEdition",
+  "Royal Revolt",
+  "Shazam",
+  "Sidia.LiveWallpaper",
+  "SlingTV",
+  "Spotify",
+  "TikTok",
+  "TuneInRadio",
+  "Twitter",
+  "Viber",
+  "WinZipUniversal",
+  "Wunderlist",
+  "XING",
+  # Not default removal
+  "Microsoft.MSPaint",
+  "Microsoft.OneDrive",
+  "Microsoft.People",
+  "Microsoft.RemoteDesktop",
+  "Microsoft.Whiteboard",
+  "Microsoft.windowscommunicationsapps"
+)
+
+# Configure and Debloat Windows using `Win11Debloat` (https://github.com/Raphire/Win11Debloat)
+$Win11Debloat = Invoke-RestMethod "https://debloat.raphi.re/"
+& ([scriptblock]::Create($Win11Debloat)) `
+  -Silent `
+  # Windows Configurations
+  -DisableTelemetry -DisableSuggestions -DisableEdgeAds `
+  -DisableDesktopSpotlight -DisableLockscreenTips `
+  -DisableBing -DisableCopilot -DisableRecall -DisableEdgeAI -DisableNotepadAI `
+  -RevertContextMenu -DisableStickyKeys `
+  -ShowHiddenFolders -ShowKnownFileExt `
+  -EnableDarkMode `
+  -CombineTaskbarAlways -CombineMMTaskbarAlways -MMTaskbarModeActive `
+  -HideSearchTb -HideTaskview -HideChat -EnableEndTask -EnableLastActiveClick `
+  -HideHome -HideGallery -ExplorerToThisPC `
+  # App Removal
+  -RemoveApps -Apps $RemoveApps
+
+
 # Todo Install WSL + Ubuntu
