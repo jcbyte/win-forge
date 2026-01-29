9e16f1b56caf8122775d9a6af9adf898ef8c18a2
Joel Cutler <joelcutler108@gmail.com>
2026-01-29 00:38:06 +0000

    enter admin in dev

--- DevSetup.ps1
+++ DevSetup.ps1
@@ -1,3 +1,9 @@
-$RepoDir = (Get-Location).Path
+
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  Start-Process -FilePath PowerShell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+  Exit
+}
+
+$RepoDir = Split-Path -Parent $PSCommandPath
 $SetupPipelineScript = Join-Path $RepoDir "SetupPipeline.ps1"
 & $SetupPipelineScript $RepoDir
