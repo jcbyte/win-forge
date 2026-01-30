9cdaa4e37e847e0b1f26e47a69801ae5bcd4e502
Joel Cutler <joelcutler108@gmail.com>
2026-01-30 15:18:04 +0000

    remove cred in install winget util

--- Utils/Utils.psm1
+++ Utils/Utils.psm1
@@ -16,9 +16,8 @@ function Sync-Path {
 }
 
 # Install WinGet packages unattended
-# $Package = {Id:string; Title?:string; Privilege?:string; Override?:string}
-# User credentials in $Cred are required when installing with "user" privilege
-function Install-WinGetUnattended([PSCustomObject]$Package, [PSCredential]$Cred = $null) {
+# $Package = {Id:string; Title?:string; Override?:string}
+function Install-WinGetUnattended([PSCustomObject]$Package) {
   Write-Host "🔷" -NoNewline -ForegroundColor DarkCyan
   Write-Host " Installing " -NoNewline
   if ($Package.Title) { Write-Host $Package.Title -ForegroundColor Cyan }
@@ -28,24 +27,6 @@ function Install-WinGetUnattended([PSCustomObject]$Package, [PSCredential]$Cred 
   $WinGetCmd = "winget install -e --id $($Package.Id) --silent --accept-source-agreements --accept-package-agreements --source winget"
   if ($Package.Override) { $WinGetCmd += " --override `"$($Package.Override)`"" }
 
-  # Default to admin privilege unless explicitly set
-  $Privilege = $Package.Privilege
-  if (-not $Privilege) { $Privilege = "admin" }
-
-  switch ($Privilege) {
-    "admin" {
-      # Perform the install in this elevated shell
-      Invoke-Expression $WinGetCmd
-    }
-    "user" { 
-      if ($Cred) {
-        # Perform the install in a new shell with user permission
-        $ArgList = @("-NoProfile", "-ExecutionPolicy", "Bypass", "-Command", $WinGetCmd)
-        Start-Process -FilePath PowerShell.exe -ArgumentList $ArgList -Credential $Cred -Wait
-      }
-      else {
-        Write-Host "Install requires user privilege, not given (ignoring)" -ForegroundColor Red
-      }
-    }
-  }
+  # Perform the install
+  Invoke-Expression $WinGetCmd
 }
