4201627b83449eb2d609adcd7fa775e56df44205
Joel Cutler <joelcutler108@gmail.com>
2026-02-07 17:24:46 +0000

    save chosen extra packages to persistent storage

--- Admin/Setup.ps1
+++ Admin/Setup.ps1
@@ -9,37 +9,34 @@ param (
 
 Import-Module (Join-Path $PSScriptRoot "..\Utils")
 
+$ExtraPackagesPath = Join-Path $Repo.Dir ".extra-packages.xml"
 
-# Ask if any extra packages should be included
-
-$ExtraPackages = @()
-
-Write-Prompt -Question "Install Nvidia GeForceExperience" { 
-  $script:ExtraPackages += [PSCustomObject]@{Id = "Nvidia.GeForceExperience"; Title = "Nvidia GeForceExperience" } 
-}
-Write-Prompt -Question "Install MSI Afterburner" { 
-  $script:ExtraPackages += [PSCustomObject]@{Id = "Guru3D.Afterburner"; Title = "MSI Afterburner" } 
-}
-Write-Prompt -Question "Install Razer Synapse" { 
-  # ? This will always install Synapse 3
-  $script:ExtraPackages += [PSCustomObject]@{Id = "RazerInc.RazerInstaller.Synapse3"; Title = "Razer Synapse 3" } 
-}
-Write-Prompt -Question "Install Corsair iCUE" { 
-  # ? This will always install iCUE 5
-  $script:ExtraPackages += [PSCustomObject]@{Id = "Corsair.iCUE.5"; Title = "Corsair iCUE" }
-}
-Write-Prompt -Question "Install Asus ArmouryCrate" {
-  $script:ExtraPackages += [PSCustomObject]@{Id = "Asus.ArmouryCrate"; Title = "Asus ArmouryCrate" }
-}
-
-
-# todo load/save this state between restart
-# todo install these
-# todo add these to postsetup todos
-
-
-# Only communicate with client before the restart
+# Perform first time steps
 if ($ResumeStep -eq 0) {
+  # Ask if any extra packages should be included
+  $ExtraPackages = @()
+  Write-Prompt -Question "Install Nvidia GeForceExperience" { 
+    $script:ExtraPackages += [PSCustomObject]@{Id = "Nvidia.GeForceExperience"; Title = "Nvidia GeForceExperience" } 
+  }
+  Write-Prompt -Question "Install MSI Afterburner" { 
+    $script:ExtraPackages += [PSCustomObject]@{Id = "Guru3D.Afterburner"; Title = "MSI Afterburner" } 
+  }
+  Write-Prompt -Question "Install Razer Synapse" { 
+    # ? This will always install Synapse 3
+    $script:ExtraPackages += [PSCustomObject]@{Id = "RazerInc.RazerInstaller.Synapse3"; Title = "Razer Synapse 3" } 
+  }
+  Write-Prompt -Question "Install Corsair iCUE" { 
+    # ? This will always install iCUE 5
+    $script:ExtraPackages += [PSCustomObject]@{Id = "Corsair.iCUE.5"; Title = "Corsair iCUE" }
+  }
+  Write-Prompt -Question "Install Asus ArmouryCrate" {
+    $script:ExtraPackages += [PSCustomObject]@{Id = "Asus.ArmouryCrate"; Title = "Asus ArmouryCrate" }
+  }
+
+  # Save them in a file so they are not lost when restarting
+  $ExtraPackages | Export-Clixml -Path $ExtraPackagesPath
+
+  # Communicate with client script
   Import-Module (Join-Path $PSScriptRoot "..\IPC")
 
   # Get Event handles shared between user and admin setup scripts
@@ -64,6 +61,10 @@ if ($ResumeStep -eq 0) {
     & $CleanupScript
     Exit
   }
+}
+else {
+  # Load the extra packages from the file
+  $ExtraPackages = Import-Clixml -Path $ExtraPackagesPath
 }
 
 # Execute each stage of the admin setup
