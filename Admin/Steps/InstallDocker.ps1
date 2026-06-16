84f14c2e43a196cfa0348ad0f920c953484c8a6e
Joel Cutler <57547887+jcbyte@users.noreply.github.com>
2026-06-16 15:48:08 +0100

    fix syntax for docker install script

--- Admin/Steps/InstallDocker.ps1
+++ Admin/Steps/InstallDocker.ps1
@@ -5,7 +5,7 @@ Import-Module (Join-Path $PSScriptRoot "..\..\Utils")
 
 # Install docker
 # This is done separately, as WSL must be enabled before it can be installed
-Install-WinGetUnattended [PSCustomObject]@ { Id = "Docker.DockerDesktop"; Title = "Docker Desktop"; Scope = "machine" }
+Install-WinGetUnattended ([PSCustomObject]@{ Id = "Docker.DockerDesktop"; Title = "Docker Desktop"; Scope = "machine" })
 
 # Synchronise the PATH, as installs may have effected it
 Sync-Path
